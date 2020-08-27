debe60ef50f74525a0ad37b6e7439b4487724a5a
Akot91 <adam.kot91@wp.pl>
2020-08-27 15:29:52 +0200

    master update

--- tasks/src/main/java/com/crud/tasks/KodillaApplication.java
+++ tasks/src/main/java/com/crud/tasks/KodillaApplication.java
@@ -3,11 +3,11 @@ package com.crud.tasks;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class KodillaApplication {
+//@SpringBootApplication
+/*public class KodillaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(KodillaApplication.class, args);
     }
 
-}
+}*/
