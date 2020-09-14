083bcab7862c06dbcc5415c7b9ad3433bf74bbd8
Akot91 <adam.kot91@wp.pl>
2020-09-14 14:06:11 +0200

    master update

--- src/test/java/com/crud/tasks/TasksApplicationTests.java
+++ src/test/java/com/crud/tasks/TasksApplicationTests.java
@@ -3,10 +3,10 @@ package com.crud.tasks;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TasksApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
