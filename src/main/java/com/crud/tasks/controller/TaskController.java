c6a3c136792e4dcd41c9e1672ccf8ad88f1dd383
Akot91 <adam.kot91@wp.pl>
2020-09-17 14:19:51 +0200

    update

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -30,7 +30,7 @@ public class TaskController {
         return taskMapper.mapToTaskDto(service.getTask(taskId).orElseThrow(TaskNotFoundException::new));
     }
 
-    @DeleteMapping("deleteTask/{taskId}")
+    @DeleteMapping("/{taskId}")
     public void deleteTask(@PathVariable long taskId) {
         service.deleteTask(taskId);
     }
