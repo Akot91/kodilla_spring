7398af7e77eb2044c17b1d0a4bb29a07d6a88c26
Akot91 <adam.kot91@wp.pl>
2020-10-13 13:51:29 +0200

    task23.4

--- tasks/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
+++ tasks/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
@@ -53,6 +53,18 @@ public class CrudAppTestSuite {
         return taskName;
     }
 
+    private void deleteCrudAppTestTask(String taskName) throws InterruptedException {
+        webDriver.findElements(By.xpath("//form[@class=\"datatable_row\"]")).stream()
+                .filter(webElement -> webElement.findElement(By.xpath(".//p[@class=\"datatable__field-value\"]"))
+                        .getText().equals(taskName))
+                .forEach(theForm -> {
+                    WebElement buttonDelete = theForm.findElement(By.xpath(".//button[@text()=\"Delete\"]"));
+                    buttonDelete.click();
+                });
+
+        Thread.sleep(2000);
+    }
+
     private void sendTestTaskToTrello(String taskName) throws InterruptedException {
         webDriver.navigate().refresh();
 
@@ -109,5 +121,6 @@ public class CrudAppTestSuite {
         String taskName = createCrudAppTestTask();
         sendTestTaskToTrello(taskName);
         assertTrue(checkTaskExistsInTrello(taskName));
+        deleteCrudAppTestTask(taskName);
     }
 }
