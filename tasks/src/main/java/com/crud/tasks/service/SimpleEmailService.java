cf11a52991840d1689e4f9fcf2994c0f68edb3df
Akot91 <adam.kot91@wp.pl>
2020-10-14 13:23:07 +0200

    master update

--- tasks/src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ tasks/src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,8 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
 
 import static org.apache.logging.log4j.util.Strings.isNotEmpty;
 
@@ -17,6 +18,9 @@ public class SimpleEmailService {
 
     @Autowired
     private JavaMailSender javaMailSender;
+
+    @Autowired
+    private MailCreatorService mailCreatorService;
 
     public static final Logger LOGGER = LoggerFactory.getLogger(SimpleEmailService.class);
 
@@ -29,6 +33,16 @@ public class SimpleEmailService {
         } catch (MailException e) {
             LOGGER.error("Failed to process email sending", e.getMessage(), e);
         }
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage);
+            mimeMessageHelper.setTo(mail.getReceiveEmial());
+            mimeMessageHelper.setCc(mail.getToCC());
+            mimeMessageHelper.setSubject(mail.getSubject());
+            mimeMessageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
     }
 
     private SimpleMailMessage createMailMessage(final Mail mail) {
